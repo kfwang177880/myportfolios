a4440769e8e05a3f4d8b03ebb6579e95a276dcf7
kfwang177880 <kfwang177880@yahoo.com>
2020-08-21 21:26:19 -0400

    Update landingpage.js, app.css

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,43 @@
 
 import React, { Component } from 'react';
+import { Grid, Cell} from 'react-mdl';
 
 class Landing extends Component {
   render() {
     return (
-      <div><h1>Landing Page</h1></div>
+      <div style= {{width: '100%', margin: 'auto'}}>  
+        <Grid className = "landing-grid">             
+          <Cell col={12}>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTXYQesvImAA5qQbRO4AExrP6VaPRHmxfScZg&usqp=CAU"
+              alt="avatar"
+              className="avatar-img"
+            />
+
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+
+              <hr/>
+     
+              <p>HTML/CSS| Bootstrap | JavaScript | React | React Native | NodeJs |</p>
+
+              <div className="social-links">
+
+                {/* linkedIN */}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+
+                {/* GitHUb */}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+
+              </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
     )
   }
 }
--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import LandingPage from './landingpage';
 import AboutMe from './aboutme';
 import Contact from './contact';
